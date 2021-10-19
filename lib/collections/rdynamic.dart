5078bdf7e25335736c4b5feefd60e0e4cd3a048b
PhotonRayleigh <PhotonRayleigh@protonmail.com>
2021-10-18 23:52:46 -0400

    Made a new RDynamic class. Provides proper and complete runtime type checking.
    With this, you can construct a type-safe object at runtime, it will guarentee
    it only ever contains objects of it's specified type.

--- lib/collections/rdynamic.dart
+++ lib/collections/rdynamic.dart
@@ -1,4 +1,4 @@
-extension RDynamic on dynamic {
+extension Dynamic on dynamic {
   E as<E>() => this as E;
 }
 
@@ -11,10 +11,60 @@ extension RList on List {
   /// to access the type of a list item.
   /// Example: myList.type[0] // returns the type of the item at index 0.
   _TypeAccess get type => _TypeAccess(this);
+
+  /// Returns true if the list matches the types specified in
+  /// the types list
+  bool checkTypes(List<Type> types) {
+    if (types.length != this.length)
+      throw "Error: types list must mach target list's length";
+    for (int i = 0; i < this.length; i++) {
+      if (this[i].runtimeType != types[i]) return false;
+    }
+    return true;
+  }
 }
 
 class _TypeAccess {
   List target;
   _TypeAccess(this.target);
   Type operator [](int index) => this[index].runtimeType;
+}
+
+class RDynamic {
+  late final Type type;
+  dynamic _value;
+  dynamic get value => _value;
+  set value(dynamic val) {
+    if (val.runtimeType == type)
+      _value = val;
+    else
+      throw "Error setting RDynamic value: type $type expected, but ${val.runtimeType} was provided.";
+  }
+
+  E valueAs<E>() => _value as E;
+  void setAs<E>(E val) {
+    if (E == type)
+      _value = val;
+    else
+      throw "Error setting RDynamic value: type $type expected, but $E was provided.";
+  }
+
+  RDynamic(this.type, [dynamic value]) {
+    this.value = value;
+  }
+  RDynamic.asNull(this.type) {
+    makeNull();
+  }
+  RDynamic.implicit(dynamic value) {
+    if (value == null) {
+      throw "Error: null cannot be used to set type implicitly in RDynamic.";
+    } else {
+      type = value.runtimeType;
+      this.value = value;
+    }
+  }
+
+  void makeNull() {
+    _value = null;
+  }
 }
