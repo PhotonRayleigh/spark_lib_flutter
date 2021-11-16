ffa3e56830fd3726b7e5b92fedd0366f98f7162a
PhotonRayleigh <PhotonRayleigh@protonmail.com>
2021-11-16 18:06:49 -0500

    Depreciated ShiftRightFixer. It seems to be fixed in Flutter now.

--- lib/widgets/shift_right_fixer.dart
+++ lib/widgets/shift_right_fixer.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/services.dart';
 
-// Fix for bug where right shift button acts like caps lock.
-// Issue and solution described here: https://github.com/flutter/flutter/issues/75675
+/// 11/16/2021 - It appears this bug was fixed in recent Flutter updates.
+/// It is no longer required for apps to work.
+///
+/// Fix for bug where right shift button acts like caps lock.
+/// Issue and solution described here: https://github.com/flutter/flutter/issues/75675
 class ShiftRightFixer extends StatefulWidget {
   ShiftRightFixer({Key? key, required this.child}) : super(key: key);
   final Widget child;
