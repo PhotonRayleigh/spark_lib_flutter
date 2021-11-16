6874fe483efbfb5674f1a801522d9ff8e2e0faca
PhotonRayleigh <PhotonRayleigh@protonmail.com>
2021-11-16 17:19:54 -0500

    Removed Flash as dependency for dialog.
    My plan is to use Flutter's built-in Material components for dialogs, snack bars,
    and banners.

--- lib/notifications/dialog.dart
+++ lib/notifications/dialog.dart
@@ -1,78 +1,99 @@
-import 'package:flash/flash.dart';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 
-// TODO: How to keep title bar drag working while dialog is active?
-Future<T?> flashDialog<T>(BuildContext context,
+import '../navigation/app_navigator.dart';
+
+Future<T?> showBaseDialog<T>(BuildContext context,
     {String? title, String? message}) async {
   Widget buildDialog(
     BuildContext context,
-    FlashController<Object?> controller,
   ) {
-    return Flash.dialog(
-      controller: controller,
-      barrierDismissible: true,
-      barrierBlur: 3.0,
-      // margin: EdgeInsets.only(left: 30, right: 30),
-      backgroundColor: Colors.transparent, //Theme.of(context).backgroundColor,
-      //borderRadius: BorderRadius.circular(6),
-      // alignment: Alignment.center,
-      child: Stack(children: [
-        ConstrainedBox(
-            constraints: BoxConstraints.expand(),
-            child: GestureDetector(
-              onTap: controller.dismiss,
-            )),
-        Center(
-            child: GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                child: Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).backgroundColor,
-                      borderRadius: BorderRadius.circular(6),
+    var headingTextStyle = Theme.of(context).textTheme.headline6!.copyWith(
+          decoration: TextDecoration.underline,
+        );
+    return Dialog(
+        insetPadding: EdgeInsets.zero,
+        backgroundColor: Colors.transparent,
+        child: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+            child: Stack(children: [
+              ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: GestureDetector(
+                    onTap: AppNavigator.safePop,
+                  )),
+              Center(
+                  child: GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints.loose(Size(
+                            MediaQuery.of(context).size.width * 0.5,
+                            MediaQuery.of(context).size.height)),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).dialogBackgroundColor,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  if (title != null)
+                                    Text(
+                                      title,
+                                      style: headingTextStyle,
+                                      textAlign: TextAlign.center,
+                                    ), // Title
+                                  if (message != null)
+                                    Text(
+                                      message,
+                                      softWrap: true,
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  TextButton(
+                                    child: Text("Ok"),
+                                    onPressed: AppNavigator.safePop,
+                                  ),
+                                ],
+                              )),
+                        ),
+                      ))),
+              if (!kIsWeb &&
+                  (Platform.isLinux || Platform.isMacOS || Platform.isWindows))
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints.tight(
+                      Size(MediaQuery.of(context).size.width, 50),
                     ),
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints.loose(Size(
-                          MediaQuery.of(context).size.width * 0.5,
-                          MediaQuery.of(context).size.height)),
-                      child: SingleChildScrollView(
-                          scrollDirection: Axis.vertical,
-                          child: Column(
-                            children: [
-                              Text(
-                                title ?? "Alert",
-                                style: Theme.of(context).textTheme.headline5,
-                                textAlign: TextAlign.center,
-                              ), // Title
-                              if (message != null)
-                                Text(
-                                  message,
-                                  softWrap: true,
-                                  textAlign: TextAlign.center,
-                                ),
-                              TextButton(
-                                child: Text("Ok"),
-                                onPressed: controller.dismiss,
-                              ),
-                            ],
-                          )),
-                    )))),
-        if (!kIsWeb &&
-            (Platform.isLinux || Platform.isMacOS || Platform.isWindows))
-          Align(
-            alignment: Alignment.topCenter,
-            child: ConstrainedBox(
-              constraints: BoxConstraints.tight(
-                Size(MediaQuery.of(context).size.width, 50),
-              ),
-              child: MoveWindow(),
-            ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: MoveWindow(),
+                        ),
+                        SizedBox(
+                          child: MinimizeWindowButton(),
+                          height: 50,
+                        ),
+                        SizedBox(height: 50, child: MaximizeWindowButton()),
+                        SizedBox(height: 50, child: CloseWindowButton()),
+                      ],
+                    ),
+                  ),
+                ),
+            ]),
           ),
-      ]),
-    );
+        ));
   }
 
-  return showFlash(context: context, builder: buildDialog);
+  return showDialog(context: context, builder: buildDialog);
 }
